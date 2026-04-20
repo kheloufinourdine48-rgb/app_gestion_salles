52846efd4ee8ba80011615736738e2ff8176218c
kheloufinourdine48-rgb <kheloufinourdine48@gmail.com>
2026-04-20 00:37:40 -0400

    afficher_infos()

--- models/salle.py
+++ models/salle.py
@@ -4,3 +4,9 @@ class Salle:
         self.libelle = libelle
         self.type = type_salle
         self.capacite = capacite
+
+    def afficher_infos(self):
+        print(f"Code: {self.code}")
+        print(f"Libellé: {self.libelle}")
+        print(f"Type: {self.type}")
+        print(f"Capacité: {self.capacite}")
