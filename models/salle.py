c49526460cd9131495355bdff119f072a96d1af7
kheloufinourdine48-rgb <kheloufinourdine48@gmail.com>
2026-04-20 00:35:37 -0400

    classe Salle

---
+++ models/salle.py
@@ -0,0 +1,6 @@
+class Salle:
+    def __init__(self, code, libelle, type_salle, capacite):
+        self.code = code
+        self.libelle = libelle
+        self.type = type_salle
+        self.capacite = capacite
