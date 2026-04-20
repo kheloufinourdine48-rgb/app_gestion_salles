5a550990525e41d7878c6bfdaf31df4378ba1616
kheloufinourdine48-rgb <kheloufinourdine48@gmail.com>
2026-04-20 00:54:20 -0400

    get_salles

--- data/dao_salle.py
+++ data/dao_salle.py
@@ -58,3 +58,13 @@ class DataSalle:
             return Salle(*result)
         return None
 
+    def get_salles(self):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT * FROM salle")
+        results = cursor.fetchall()
+        conn.close()
+
+        return [Salle(*row) for row in results]
+
