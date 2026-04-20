f4ba2c9dee161bed2b3a87dbdccdec648a8de2dd
kheloufinourdine48-rgb <kheloufinourdine48@gmail.com>
2026-04-20 01:37:42 -0400

    la méthode  rechercher_salle

--- views/view_salle.py
+++ views/view_salle.py
@@ -67,3 +67,18 @@ class ViewSalle(ctk.CTk):
         code = self.entry_code.get()
         self.service.supprimer_salle(code)
         self.lister()
+
+
+    def rechercher(self):
+        code = self.entry_code.get()
+        salle = self.service.rechercher_salle(code)
+
+        if salle:
+            self.entry_libelle.delete(0, "end")
+            self.entry_libelle.insert(0, salle.libelle)
+
+            self.entry_type.delete(0, "end")
+            self.entry_type.insert(0, salle.type)
+
+            self.entry_capacite.delete(0, "end")
+            self.entry_capacite.insert(0, salle.capacite)
