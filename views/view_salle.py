7f1fe4eee4c4a825251cee69581e66b465e562d7
kheloufinourdine48-rgb <kheloufinourdine48@gmail.com>
2026-04-20 01:28:26 -0400

    Création de la classe ViewSalle

---
+++ views/view_salle.py
@@ -0,0 +1,14 @@
+import customtkinter as ctk
+from tkinter import ttk
+from models.salle import Salle
+from services.services_salle import ServiceSalle
+
+class ViewSalle(ctk.CTk):
+
+    def __init__(self):
+        super().__init__()
+
+        self.title("Gestion des salles")
+        self.geometry("700x500")
+
+        self.service = ServiceSalle()
