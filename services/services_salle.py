1c890ac4695952b991e8d2ba50d29707af9a3e76
kheloufinourdine48-rgb <kheloufinourdine48@gmail.com>
2026-04-20 01:06:50 -0400

    Méthode ajouter_salle

--- services/services_salle.py
+++ services/services_salle.py
@@ -4,3 +4,13 @@ class ServiceSalle:
 
     def __init__(self):
         self.dao = DataSalle()
+
+    def ajouter_salle(self, salle):
+        if not all([salle.code, salle.libelle, salle.type, salle.capacite]):
+            return False, "Tous les champs sont obligatoires"
+
+        if int(salle.capacite) < 1:
+            return False, "Capacité invalide"
+
+        self.dao.insert_salle(salle)
+        return True, "Salle ajoutée"
